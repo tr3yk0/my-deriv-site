093c5db8e59a05906b0f45056957220848ff441a
karisa-jpg <faviokarisa@gmail.com>
2026-05-18 07:34:40 +0300

    Update jest.config.js to include svg mock

--- __mocks__/jest.config.js
+++ __mocks__/jest.config.js
@@ -1,6 +1,8 @@
 module.exports = {
   moduleNameMapper: {
-    '\\.(jpg|jpeg|png|gif|svg)$': '<rootDir>/__mocks__/fileMock.js',
+    '\\.(jpg|jpeg|png|gif)$': '<rootDir>/__mocks__/fileMock.js',
     '\\.(css|less|scss|sass)$': '<rootDir>/__mocks__/styleMock.js',
+    '\\.svg$': '<rootDir>/__mocks__/svgMock.js',
   },
+  testEnvironment: 'jsdom',
 };
