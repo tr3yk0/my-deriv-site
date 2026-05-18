67be137da64dd2f89f8f11b4479c1d84ea91fc0a
karisa-jpg <faviokarisa@gmail.com>
2026-05-18 07:32:24 +0300

    Add jest.config.js mock configuration

---
+++ __mocks__/jest.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  moduleNameMapper: {
+    '\\.(jpg|jpeg|png|gif|svg)$': '<rootDir>/__mocks__/fileMock.js',
+    '\\.(css|less|scss|sass)$': '<rootDir>/__mocks__/styleMock.js',
+  },
+};
