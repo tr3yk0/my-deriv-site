1f4801b6ab2ba97036557ab7e2207a301dfd1bfe
treyko <faviokarisa@gmail.com>
2026-05-21 20:55:09 +0300

    Add files via upload

---
+++ src/external/bot-skeleton /scratch/hooks/colours.js
@@ -0,0 +1,41 @@
+const lightMode = () => {
+    const workspace = Blockly;
+    workspace.Colours.RootBlock = {
+        // Softer mid-blue to improve contrast with the workspace background
+        colour: '#0d5db8',
+        colourSecondary: '#0d5db8',
+        colourTertiary: '#0c4c99',
+    };
+
+    workspace.Colours.Base = {
+        colour: '#e5e5e5',
+        colourSecondary: '#ffffff',
+        colourTertiary: '#6d7278',
+    };
+
+    workspace.Colours.Special1 = {
+        colour: '#e5e5e5',
+        colourSecondary: '#ffffff',
+        colourTertiary: '#6d7278',
+    };
+
+    workspace.Colours.Special2 = {
+        colour: '#e5e5e5',
+        colourSecondary: '#ffffff',
+        colourTertiary: '#6d7278',
+    };
+
+    workspace.Colours.Special3 = {
+        colour: '#e5e5e5',
+        colourSecondary: '#ffffff',
+        colourTertiary: '#6d7278',
+    };
+
+    workspace.Colours.Special4 = {
+        colour: '#e5e5e5',
+        colourSecondary: '#000000',
+        colourTertiary: '#0e0e0e',
+    };
+};
+
+export const setColors = () => lightMode();
