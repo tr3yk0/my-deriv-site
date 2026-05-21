ba8653b62a120f445ca2d512030644e1964eedee
treyko <faviokarisa@gmail.com>
2026-05-21 21:04:38 +0300

    Add index.js to import various modules

---
+++ src/external/bot-skeleton /scratch/blocks /index.js
@@ -0,0 +1,21 @@
+import './Advanced/Functions';
+import './Advanced/List';
+import './Advanced/Loops';
+import './Advanced/Variable';
+import './Binary/After Purchase';
+import './Binary/Before Purchase';
+import './Binary/During Purchase';
+import './Binary/Indicators';
+import './Binary/Tick Analysis';
+import './Binary/Analysis Logics';
+import './Binary/Tools/Candle';
+import './Binary/Tools/Misc';
+import './Binary/Tools/Time';
+import './Binary/Contract Modifiers';
+import './Binary/Binarytools';
+
+import './Binary/Trade Definition';
+import './Logic';
+import './Math';
+import './Text';
+import './VirtualHook';
