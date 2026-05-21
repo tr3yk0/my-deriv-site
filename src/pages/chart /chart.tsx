9a7cb8726b147e3ca5ad7949df09c336bae5ba15
treyko <faviokarisa@gmail.com>
2026-05-21 07:41:43 +0300

    Add Chart component with real-time data handling

---
+++ src/pages/chart /chart.tsx
@@ -0,0 +1,160 @@
+import { useEffect, useRef, useState } from 'react';
+import classNames from 'classnames';
+import { observer } from 'mobx-react-lite';
+import chart_api from '@/external/bot-skeleton/services/api/chart-api';
+import { useStore } from '@/hooks/useStore';
+import {
+    ActiveSymbolsRequest,
+    ServerTimeRequest,
+    TicksHistoryResponse,
+    TicksStreamRequest,
+    TradingTimesRequest,
+} from '@deriv/api-types';
+import { ChartTitle, SmartChart } from '@deriv/deriv-charts';
+import { useDevice } from '@deriv-com/ui';
+import ToolbarWidgets from './toolbar-widgets';
+import '@deriv/deriv-charts/dist/smartcharts.css';
+
+type TSubscription = {
+    [key: string]: null | {
+        unsubscribe?: () => void;
+    };
+};
+
+type TError = null | {
+    error?: {
+        code?: string;
+        message?: string;
+    };
+};
+
+const subscriptions: TSubscription = {};
+
+const Chart = observer(({ show_digits_stats }: { show_digits_stats: boolean }) => {
+    const barriers: [] = [];
+    const { common, ui } = useStore();
+    const { chart_store, run_panel, dashboard } = useStore();
+    const [isSafari, setIsSafari] = useState(false);
+
+    const {
+        chart_type,
+        getMarketsOrder,
+        granularity,
+        onSymbolChange,
+        setChartStatus,
+        symbol,
+        updateChartType,
+        updateGranularity,
+        updateSymbol,
+        setChartSubscriptionId,
+        chart_subscription_id,
+    } = chart_store;
+    const chartSubscriptionIdRef = useRef(chart_subscription_id);
+    const { isDesktop, isMobile } = useDevice();
+    const { is_drawer_open } = run_panel;
+    const { is_chart_modal_visible } = dashboard;
+    const settings = {
+        assetInformation: false, // ui.is_chart_asset_info_visible,
+        countdown: true,
+        isHighestLowestMarkerEnabled: false, // TODO: Pending UI,
+        language: common.current_language.toLowerCase(),
+        position: ui.is_chart_layout_default ? 'bottom' : 'left',
+        theme: ui.is_dark_mode_on ? 'dark' : 'light',
+    };
+
+    useEffect(() => {
+        // Safari browser detection
+        const isSafariBrowser = () => {
+            const ua = navigator.userAgent.toLowerCase();
+            return ua.indexOf('safari') !== -1 && ua.indexOf('chrome') === -1 && ua.indexOf('android') === -1;
+        };
+
+        setIsSafari(isSafariBrowser());
+
+        return () => {
+            chart_api.api.forgetAll('ticks');
+        };
+    }, []);
+
+    useEffect(() => {
+        chartSubscriptionIdRef.current = chart_subscription_id;
+    }, [chart_subscription_id]);
+
+    useEffect(() => {
+        if (!symbol) updateSymbol();
+    }, [symbol, updateSymbol]);
+
+    const requestAPI = (req: ServerTimeRequest | ActiveSymbolsRequest | TradingTimesRequest) => {
+        return chart_api.api.send(req);
+    };
+    const requestForgetStream = (subscription_id: string) => {
+        subscription_id && chart_api.api.forget(subscription_id);
+    };
+
+    const requestSubscribe = async (req: TicksStreamRequest, callback: (data: any) => void) => {
+        try {
+            requestForgetStream(chartSubscriptionIdRef.current);
+            const history = await chart_api.api.send(req);
+            setChartSubscriptionId(history?.subscription.id);
+            if (history) callback(history);
+            if (req.subscribe === 1) {
+                subscriptions[history?.subscription.id] = chart_api.api
+                    .onMessage()
+                    ?.subscribe(({ data }: { data: TicksHistoryResponse }) => {
+                        callback(data);
+                    });
+            }
+        } catch (e) {
+            // eslint-disable-next-line no-console
+            (e as TError)?.error?.code === 'MarketIsClosed' && callback([]); //if market is closed sending a empty array  to resolve
+            console.log((e as TError)?.error?.message);
+        }
+    };
+
+    if (!symbol) return null;
+    const is_connection_opened = !!chart_api?.api;
+    return (
+        <div
+            className={classNames('dashboard__chart-wrapper', {
+                'dashboard__chart-wrapper--expanded': is_drawer_open && isDesktop,
+                'dashboard__chart-wrapper--modal': is_chart_modal_visible && isDesktop,
+                'dashboard__chart-wrapper--safari': isSafari,
+            })}
+            dir='ltr'
+        >
+            <SmartChart
+                id='dbot'
+                barriers={barriers}
+                showLastDigitStats={show_digits_stats}
+                chartControlsWidgets={null}
+                enabledChartFooter={false}
+                chartStatusListener={(v: boolean) => setChartStatus(!v)}
+                toolbarWidget={() => (
+                    <ToolbarWidgets
+                        updateChartType={updateChartType}
+                        updateGranularity={updateGranularity}
+                        position={!isDesktop ? 'bottom' : 'top'}
+                        isDesktop={isDesktop}
+                    />
+                )}
+                chartType={chart_type}
+                isMobile={isMobile}
+                enabledNavigationWidget={isDesktop}
+                granularity={granularity}
+                requestAPI={requestAPI}
+                requestForget={() => {}}
+                requestForgetStream={() => {}}
+                requestSubscribe={requestSubscribe}
+                settings={settings}
+                symbol={symbol}
+                topWidgets={() => <ChartTitle onChange={onSymbolChange} />}
+                isConnectionOpened={is_connection_opened}
+                getMarketsOrder={getMarketsOrder}
+                isLive
+                leftMargin={80}
+            />
+        </div>
+    );
+});
+
+export default Chart;
